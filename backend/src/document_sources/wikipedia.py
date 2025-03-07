fdeab1a27cc3ebb6006b71466f0463cbd404d5fb
Tathagata Talukdar <tathagata.talukdar@gmail.com>
2025-03-07 11:48:20 +0530

    Modified code to fetch one complete page from wikipedia instead of small part from multiple pages

--- backend/src/document_sources/wikipedia.py
+++ backend/src/document_sources/wikipedia.py
@@ -4,7 +4,9 @@ from src.shared.llm_graph_builder_exception import LLMGraphBuilderException
 
 def get_documents_from_Wikipedia(wiki_query:str, language:str):
   try:
-    pages = WikipediaLoader(query=wiki_query.strip(), lang=language, load_all_available_meta=False).load()
+    pages = WikipediaLoader(query=wiki_query.strip(), lang=language, load_all_available_meta=False, load_max_docs=1, doc_content_chars_max=120000).load()
+    print("\n\n\n\n\n\nNumber of pages:", len(pages))
+    print("Page content size:", len(pages[0].page_content))
     file_name = wiki_query.strip()
     logging.info(f"Total Pages from Wikipedia = {len(pages)}") 
     return file_name, pages
@@ -13,4 +15,3 @@ def get_documents_from_Wikipedia(wiki_query:str, language:str):
     error_message = str(e)
     logging.exception(f'Failed To Process Wikipedia Query: {file_name}, Exception Stack trace: {error_message}')
     raise LLMGraphBuilderException(error_message+' '+message)
-  
